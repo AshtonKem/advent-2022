14ebf4a04154416f61479d64e17e9f263f906893
Ashton Kemerling <ashton.kemerling@protonmail.com>
2022-12-08 09:25:37 -0700

    Day six bonus.

--- src/days/day_six.rs
+++ src/days/day_six.rs
@@ -8,13 +8,13 @@ fn find_start_packet_index(input: String, header_size: usize) -> usize {
     let mut ring_buffer: VecDeque<char> = VecDeque::new();
     for (index, character) in input.chars().enumerate() {
         ring_buffer.push_front(character);
-        ring_buffer.truncate(4);
+        ring_buffer.truncate(header_size);
         let char_count = ring_buffer.iter().unique().count();
         if char_count == header_size {
             return index + 1;
         }
     }
-    0
+    input.len()
 }
 
 #[cfg(test)]
@@ -23,17 +23,47 @@ mod tests {
 
     #[test]
     pub fn test_start_packet_index() {
-        assert_eq!(5, find_start_packet_index("bvwbjplbgvbhsrlpgdmjqwftvncz".to_string(), 4));
-        assert_eq!(6, find_start_packet_index("nppdvjthqldpwncqszvftbrmjlhg".to_string(), 4));
-        assert_eq!(10, find_start_packet_index("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".to_string(), 4));
-        assert_eq!(11, find_start_packet_index("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_string(), 4));
+        assert_eq!(
+            5,
+            find_start_packet_index("bvwbjplbgvbhsrlpgdmjqwftvncz".to_string(), 4)
+        );
+        assert_eq!(
+            6,
+            find_start_packet_index("nppdvjthqldpwncqszvftbrmjlhg".to_string(), 4)
+        );
+        assert_eq!(
+            10,
+            find_start_packet_index("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".to_string(), 4)
+        );
+        assert_eq!(
+            11,
+            find_start_packet_index("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_string(), 4)
+        );
+    }
+
+    #[test]
+    pub fn test_start_message() {
+        let header_size = 14;
+        assert_eq!(
+            19,
+            find_start_packet_index("mjqjpqmgbljsphdztnvjfqwrcgsmlb".to_string(), header_size)
+        );
+        assert_eq!(
+            23,
+            find_start_packet_index("bvwbjplbgvbhsrlpgdmjqwftvncz".to_string(), header_size)
+        );
+        assert_eq!(
+            23,
+            find_start_packet_index("nppdvjthqldpwncqszvftbrmjlhg".to_string(), header_size)
+        );
     }
 }
 
-pub fn run(path: &PathBuf, _bonus_: bool) -> usize {
+pub fn run(path: &PathBuf, bonus: bool) -> usize {
     if let Ok(lines) = read_lines(path) {
         if let Some(Ok(input)) = lines.into_iter().next() {
-            find_start_packet_index(input, 4)
+            let header_size = if bonus { 14 } else { 4 };
+            find_start_packet_index(input, header_size)
         } else {
             panic!("Expected a non-empty file");
         }
